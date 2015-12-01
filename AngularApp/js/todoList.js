61cdd0d7b77ac3075410b1fce97108845c980378
Pierre <pierre.dugue1@gmail.com>
2015-12-02 10:47:17 +1100

    AngularTuto

---
+++ AngularApp/js/todoList.js
@@ -0,0 +1,11 @@
+// js/todoList.js
+'use strict';
+
+
+/**
+ * Déclaration de l'application demoApp
+ */
+var demoApp = angular.module('demoApp', [
+    // Dépendances du "module"
+    'todoList'
+]);
