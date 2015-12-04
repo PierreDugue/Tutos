a523619f02afd708083999ad9ba3b88be5362555
Pierre <pierre.dugue1@gmail.com>
2015-12-04 13:41:15 +1100

    Maj personnal website

---
+++ SitePerso/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $(".selectorNavbar").fadeTo(0, 0.15);
+    $(".selectorNavbar").mouseover(function () {
+        $(this).stop(true, true).fadeTo("slow", 1);
+    });
+    $(".selectorNavbar").mouseleave(function () {
+        $(this).stop(true, true).fadeTo(0.1, 0.15);
+    });
+});
+
+$(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
