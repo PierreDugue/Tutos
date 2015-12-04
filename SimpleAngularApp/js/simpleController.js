431951c4069f7625cd3624f7b878da4ccbaabf93
Pierre <pierre.dugue1@gmail.com>
2015-12-04 11:36:19 +1100

    End tuto

--- SimpleAngularApp/js/simpleController.js
+++ SimpleAngularApp/js/simpleController.js
@@ -1,13 +1,13 @@
 var myDemoApp = angular.module('myDemoApp', ['ngRoute']);
 
-myDemoApp.config(function ($routeProvider){
+myDemoApp.config(function ($routeProvider) {
 	$routeProvider
 		.when('/',
 			{
 				controller: "mySimpleController",
 				templateUrl: "Partials/View1.html"
 			})
-		.when("/",
+		.when('/view2',
 			{
 				controller: "mySimpleController",
 				templateUrl: "Partials/View2.html"
@@ -15,12 +15,31 @@ myDemoApp.config(function ($routeProvider){
 		.otherwise({ redirectTo: '/' });
 });
 
-var controllers = {};
-controllers.mySimpleController = function ($scope) {
-	$scope.names = [
+myDemoApp.factory('simpleFactory', function(){
+	var customers =  [
 		{ name: 'Marcel Prout', city: 'Nice' },
 		{ name: 'Marcel Gogo', city: 'Turin' },
 		{ name: 'Gogo Prout', city: 'Nice' }];
+	var factory = {};
+	factory.getCustomers =  function(){
+		return customers;
+	};
+	factory.setCustomers = function(customer){
+		
+	}
+	return factory;
+});
+
+var controllers = {};
+controllers.mySimpleController = function ($scope, simpleFactory) {
+	$scope.names = [];
+	
+	
+	$scope.names = simpleFactory.getCustomers();
+	
+	$scope.addCustomer = function () {
+		$scope.names.push({name: $scope.newCustomer.name, city: $scope.newCustomer.city});
+	}
 };
 
 myDemoApp.controller(controllers);
