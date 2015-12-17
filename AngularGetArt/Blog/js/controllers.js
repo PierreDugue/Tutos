d2c256155e5b77cd7267419dec3b65caff0105a1
Pierre <pierre.dugue1@gmail.com>
2015-12-17 16:17:54 +1100

    Ajout images

--- AngularGetArt/Blog/js/controllers.js
+++ AngularGetArt/Blog/js/controllers.js
@@ -3,8 +3,8 @@ blogApp.controller('afficheMain', function ($scope, mainFactory) {
 	$scope.articles = [];
 	$scope.articles = mainFactory.getArticles();
 	
-	$scope.addNewArticle = function ($scope) {
-		$scope.articles.push({title: $scope.article.title, text: $scope.article.text});
+	$scope.addNewArticle = function () {
+		$scope.articles.push({title: $scope.article.title, text: $scope.article.text, img: $scope.article.img});
 	}
 
 });
--- AngularGetArt/Blog/js/factories.js
+++ AngularGetArt/Blog/js/factories.js
@@ -1,7 +1,5 @@
 blogApp.factory('mainFactory', function($http){
 	var articles = [
-		{title : "1er Article", text : "Le text de l'article 1"},
-		{title : "1er Article", text : "Le text de l'article 1"}
 	];
 	
 	var factory = {};
